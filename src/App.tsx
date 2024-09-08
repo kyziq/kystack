ed1909385778f3bbc2be9c444db53c4bd25f9795
Haziq <ihaziqkhairi@gmail.com>
2024-09-08 12:44:07 +0800

    feat: add `shadcn/ui`

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,6 @@ declare module '@tanstack/react-router' {
 
 const App = () => {
   return <RouterProvider router={router} />;
-  // return (
-  //   <div className="h-screen flex items-center justify-center">
-  //     <h1 className="text-4xl font-bold">Kystack</h1>
-  //   </div>
-  // );
 };
 
 export default App;
